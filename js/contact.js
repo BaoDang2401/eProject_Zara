564f30275194cc7ac47d8cac1860a10cc3e2618d
Pearlicia <suonghbts2302004@fpt.edu.vn>
2023-06-02 20:30:56 +0700

    Boi Suong updated 62 8:30 PM
    Boi Suong updated 62 8:30 PM

--- js/contact.js
+++ js/contact.js
@@ -4,61 +4,47 @@ function Submit() {
     var tel = document.getElementById("tel").value;
     var email = document.getElementById("Email").value;
     var message = document.getElementById("messageInput").value;
-
+    const errorMessage = document.getElementById("NameError");
+    const errorMessageemail = document.getElementById("PhoneError");
+    const errorMessagep = document.getElementById("EmailError");
+    const errorMessagepp = document.getElementById("messageError");
     // Xóa thông báo lỗi trước đó
-    var errorMessages = document.getElementsByClassName("error-message");
-    for (var i = 0; i < errorMessages.length; i++) {
-        errorMessages[i].innerHTML = "";
-    }
+    // var errorMessages = document.getElementsByClassName("error-message");
+    // for (var i = 0; i < errorMessages.length; i++) {
+    //     errorMessages[i].innerHTML = "";
+    // }
 
-    // Kiểm tra xem tên, sđt, email và tin nhắn có được nhập hay không
-    var hasError = false;
-
-    if (!name) {
-        document.getElementById("NameError").innerHTML = "Please enter your full name.";
-        hasError = true;
+    if (name === "") {
+        errorMessage.innerHTML = "Please enter your full name.";
     }
-    if (!tel) {
-        document.getElementById("PhoneError").innerHTML = "Please enter your phone number.";
-        hasError = true;
+    if (tel === "") {
+        errorMessageemail.innerHTML = "Please enter your phone number.";
     } else if (!/^\+84[0-9]{9}$/.test(tel)) {
-        document.getElementById("PhoneError").innerHTML = "Format: +84 xxx.xxx.xxx";
-        hasError = true;
+        errorMessageemail.innerHTML = "Format: +84 xxx.xxx.xxx";
     }
-    if (!email) {
-        document.getElementById("EmailError").innerHTML = "Please enter your email.";
-        hasError = true;
+    if (email === "") {
+        errorMessagep.innerHTML = "Please enter your email.";
     } else if (!/^[a-zA-Z0-9]{4,}@[a-zA-Z0-9]{4,}\.[a-zA-Z0-9]{3,}/.test(email)) {
-        document.getElementById("EmailError").innerHTML = "Format: abc1@gmail.com";
-        hasError = true;
+        errorMessagep.innerHTML = "Format: abc1@gmail.com";
     }
     if (message === "") {
-        document.getElementById("messageError").innerHTML = "Please enter a message.";
-        hasError = true;
-    }
-
-    // Kiểm tra xem có lỗi hay không
-    if (hasError) {
-        document.getElementById("errorMessage").innerHTML = "Please fill in all required fields.";
-        return;
-        // Dừng lại nếu có lỗi để ngăn người dùng bấm Submit
+        errorMessagepp.innerHTML = "Please enter a message.";
     }
 
     // alert("check eror")
+    if (name && tel && email && message) {
+        var successMessage =
+            "<br>Full Name: " + name +
+            "<br>Phone: " + tel +
+            "<br>Email: " + email +
+            "<br>Message: " + message;
 
-    var successMessage =
-        "<br>Full Name: " + name +
-        "<br>Phone: " + tel +
-        "<br>Email: " + email +
-        "<br>Message: " + message;
-
-    document.getElementById("successMessage").innerHTML = successMessage;
-    $('#successModal').modal('show');
-    // Reset giá trị của các trường input sau khi xử lý form
-    document.getElementById("FullName").value = "";
-    document.getElementById("Phone").value = "";
-    document.getElementById("Email").value = "";
-    document.getElementById("messageInput").value = "";
+        document.getElementById("successMessage").innerHTML = successMessage;
+        $('#successModal').modal('show');
+        // Reset giá trị của các trường input sau khi xử lý form
+        document.getElementById("FullName").value = "";
+        document.getElementById("Phone").value = "";
+        document.getElementById("Email").value = "";
+        document.getElementById("messageInput").value = "";
+    }
 }
-
-
